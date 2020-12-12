5e8d5cffd059f25c2ed658bdc78349fdc749ec3e
unknown <asmaakhaled603@gmail.com>
2020-12-12 03:02:17 +0200

    Edit task A.3

--- JavaScript/DOM/A.3/JS/movePic.js
+++ JavaScript/DOM/A.3/JS/movePic.js
@@ -1,19 +1,31 @@
 function move(){
-    var image1,image2,image3;
-    if(document.getElementById("Mover").value=="Go"){
+    var image1,image2,image3,timer;
+    
         document.getElementById("Mover").value="Stop";
-        image1=document.getElementsByClassName("img1");
-        image2=document.getElementsByClassName("img2");
-        image3=document.getElementsByClassName("img3");
-        image3.style.top=(parseInt(getComputedStyle(image3).getPropertyValue('top')) - 10) + "px";
-                         //(parseInt(getComputedStyle(image3).top) - 10) + "px";
+        image1=document.getElementsByClassName("img1")[0];
+        image2=document.getElementsByClassName("img2")[0];
+        image3=document.getElementsByClassName("img3")[0];
         
-        //console.log("tiger" + img.style.top);
-        if(parseInt(image3.style.top)<=0){
+    
+    
+        image1.style.left=(parseInt(getComputedStyle(image1).getPropertyValue('left')) + 10) + "px";
+        image2.style.left=(parseInt(getComputedStyle(image2).getPropertyValue('left')) - 10) + "px";
+        image3.style.top=(parseInt(getComputedStyle(image3).getPropertyValue('top')) - 10) + "px";
+         console.log("img1" + image1.style.left);
+         console.log("img2" + image2.style.left);
+         console.log("img3" + image3.style.top);
+        if ((parseInt(getComputedStyle(image1).getPropertyValue('left')) >= 250) && 
+            (parseInt(getComputedStyle(image2).getPropertyValue('left')) <= 0) && 
+            (parseInt(getComputedStyle(image3).getPropertyValue('top')) <= 0)
+           ){
             clearTimeout(timer);
         }
-        var timer=setTimeout(move,50);
-    }
+         else
+         timer=setTimeout(move,50);
+   
+
+
+
 }
 function reset(){
     
